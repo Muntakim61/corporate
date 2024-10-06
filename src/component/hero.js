a5b006b45c79b71845c4b4dfac6f05f4f72ea2a1
Muntakim61 <mostafa.muntakim61@gmail.com>
2024-10-06 15:03:36 +0600

    Made minor changes to hero banner

--- src/component/hero.js
+++ src/component/hero.js
@@ -3,22 +3,22 @@ import Carousel from 'react-bootstrap/Carousel';
 var heroData = [
   {
     id: 1,
-    image: require('../assets/images/bg-img-1.jpg'),
+    image: require('../assets/images/h-bg-4.jpg'),
     title: 'The perfect design for your website',
     description: 'Custom-crafted designs that capture your brand’s essence, ensuring your website stands out and impresses.',
-    link: 'https://www.google.com'
+    link: 'https://bibekshakya.com/demo/react/reactbootstrap/corporate/'
   },
   {
     id: 2,
     // image: require('../assets/images/img-hero2.jpg'),
-    image: require('../assets/images/rbg-testimonial.jpg'),
+    image: require('../assets/images/h-bg-3.jpg'),
     title: 'Start Your Future Financial Plan',
     description: 'Take control of your financial future today with expert guidance and a plan that grows with you.',
     link: 'https://www.facebook.com'
   },
   {
     id: 3,
-    image: require('../assets/images/rbg-image-1.jpg'),
+    image: require('../assets/images/h-bg-2.jpg'),
     title: 'Enjoy the Difference',
     description: 'Experience innovation and service that sets us apart—where excellence isn’t just expected, it’s guaranteed.',
     link: 'https://www.twitter.com'
@@ -37,6 +37,8 @@ function AppHero() {
                     className="d-block w-100"
                     src={hero.image}
                     alt={"slide " + hero.id}
+                    style={{ filter: "blur(5px)" }}
+                    // style={{opacity: 0.5}}
                   />
                   <Carousel.Caption>
                     <h2>{hero.title}</h2>
